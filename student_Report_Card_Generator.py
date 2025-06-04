569d1a3f2a281449de22677d8a6b0c5cefe23bfb
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-04 17:57:36 +0530

    initiate code , unpack students as well as scores

---
+++ student_Report_Card_Generator.py
@@ -0,0 +1,24 @@
+# STUDENT REPORT CARD GENERATOR USING PYTHON tuple
+
+# 📄 Report Card for Alice (Roll No: 101)
+# - Scores: Math: 85, Science: 90, English: 78
+# - Average: 84.33
+# - Hobbies: painting, reading
+
+# -----------------------------------
+
+# Class Topper: Charlie (92.00 average)
+
+students = [
+    ("Alice", (85, 90, 78), 101, "painting", "reading"),
+    ("Bob", (72, 88, 91), 102, "chess"),
+    ("Charlie", (95, 92, 89), 103, "football", "gaming")
+]
+
+# Accessing each item of tuple using loops
+for name, scores, enroll, *hobbies in students:
+    print(f"\n📄 Repost Card for {name} (Roll No: {enroll})")
+
+    # unpacking scores (tuple)
+    math, sci, eng = scores
+    print(f"- Scores : Math : {math}, Science: {sci}, English : {eng}")
